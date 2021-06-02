eb1c25b2ee9d78ecd31e98f274d5657a7bd9c39c
Heider Lopes <heidertreinamentos@gmail.com>
2021-06-01 23:27:30 -0300

    Criação da base do appiada

---
+++ app/src/main/java/br/com/heiderlopes/appiadas/MainActivity.kt
@@ -0,0 +1,36 @@
+package br.com.heiderlopes.appiadas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var tvJoker : TextView
+    lateinit var btTellJoker: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvJoker = findViewById(R.id.tvJoker)
+        btTellJoker = findViewById(R.id.btTellJoker)
+
+        showJoker()
+
+        btTellJoker.setOnClickListener {
+            showJoker()
+        }
+    }
+
+    private fun showJoker() {
+        val jokers = resources.getStringArray(R.array.jokers)
+        val numberJoker = Random().nextInt(jokers.size)
+        val joker = jokers[numberJoker]
+        tvJoker.text = joker
+    }
+
+}
