666a423c3d72c744d19648552bc84dd731132726
Heider Pinholi Lopes <heidertreinamentos@gmail.com>
2022-04-12 15:34:18 -0300

    Teste

--- app/src/main/java/br/com/heiderlopes/appiadas/MainActivity.kt
+++ app/src/main/java/br/com/heiderlopes/appiadas/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var tvJoker : TextView
     lateinit var btTellJoker: Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
